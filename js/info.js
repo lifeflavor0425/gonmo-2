9350c858e1266c178e242180a53c1913cb62cdbd
lifeflavor0425 <rjsdnakfmzh61@gmail.com>
2021-07-01 15:13:13 +0900

    login...

---
+++ js/info.js
@@ -0,0 +1,15 @@
+class User {
+  constructor (username, email, server, phone){
+    this.username=username;
+    this.email=email;
+    this.server=sever;
+    this.phone=phone;
+  }
+  function (username, email, server, phone) {
+
+  }
+}
+
+
+let username=document.querySelector('#username').innerHTML
+console.log(username)
